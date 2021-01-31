72d30c246732da1d0ec883dc27ef02577bca9189
Alex <alexfrancisaaron@gmail.com>
2021-01-31 16:44:25 -0600

    commit tree f48188651e5b4c66f4f5ff46089f0e7e9a7abbe7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,51 @@ class ApplicationController < Sinatra::Base
 
   # code actions here!
 
+  get '/recipes/new' do
+    erb :new
+  end
+
+  get '/recipes' do 
+    @recipes = Recipe.all
+
+    erb :index
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+
+    erb :recipe
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+
+    erb :edit
+  end
+
+
+  patch '/recipes/:id' do 
+    @recipe = Recipe.find_by_id(params[:id])
+
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+
+    @recipe.save
+
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  post '/recipes' do 
+    @recipe = Recipe.create(params)
+    #@recipe = Recipe.find_or_create_by(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id' do 
+    @recipe = Recipe.find(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
+  end
+
 end
